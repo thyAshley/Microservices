d8e610d6469832f8bd7f742501a25e814e5939de
Ashley <thy.ashley@gmail.com>
2021-02-20 15:07:59 +0800

    added receiver and sender for rabbitmq

--- rabbitMQ/receive.ts
+++ rabbitMQ/receive.ts
@@ -1,12 +1,20 @@
 // docker run --rm -it --hostname my-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
 import amqplib from "amqplib";
 
-const queueName = "Hello";
+const queueName = "wdj";
 
-const sendMessage = async () => {
+const getMessage = async () => {
   const connection = await amqplib.connect("amqp://localhost");
   const channel = await connection.createChannel();
   await channel.assertQueue(queueName, { durable: false });
+  console.log(`Waiting for messages in queue: ${queueName}`);
+  channel.consume(
+    queueName,
+    (message) => {
+      console.log(`[X] Received: ${message?.content.toString()}`);
+    },
+    { noAck: true }
+  );
 };
 
-sendMessage();
+getMessage();
